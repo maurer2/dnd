a8e478539a4d94e924958b9f338b4f558338e8a0
Martin <mr.mmaurer+work@gmail.com>
2024-12-09 16:17:31 +0100

    Added basic auth for endpoint

--- docker/Readme.md
+++ docker/Readme.md
@@ -4,3 +4,7 @@
 
 * http://localhost:3000/
 * http://localhost:3000/name/:name -> http://localhost:3000/name/mechthild
+
+## Basic auth
+
+Password: 123456
--- docker/index.ts
+++ docker/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 
 const app = express();
 const port = process.env.PORT;
+const passphrase = process.env.PASSPHRASE;
 
 app.use(cors());
 
@@ -10,8 +11,33 @@ app.get("/", (req, res) => {
   res.send("Hallo Welt");
 });
 
-app.get("/name/:name", (req, res) => {
+app.get("/name/:name", (req, res, next) => {
   const { name } = req.params;
+  const authorization = req.get("authorization");
+
+  if (!authorization) {
+    res
+      .setHeader("WWW-Authenticate", "Basic")
+      .status(401)
+      .send("Access denied");
+
+    return next();
+  }
+
+  const decodedCredentials = Buffer.from(authorization.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+  const { 1: password } = decodedCredentials;
+
+  if (password !== passphrase) {
+    res
+      .setHeader("WWW-Authenticate", "Basic")
+      .status(401)
+      .send("Access denied");
+
+    return next();
+  }
+
   const dateNow = new Date();
 
   const nameFormatted = `${name.charAt(0).toUpperCase()}${name.slice(1)}`;
