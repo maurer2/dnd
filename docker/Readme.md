abee2c4edf7f64cf6ceb629d55056f46f606440a
Martin <mr.mmaurer+work@gmail.com>
2024-12-09 15:28:16 +0100

    Added endpoint with param

---
+++ docker/Readme.md
@@ -0,0 +1,6 @@
+# Simple express app in docker
+
+## URLS
+
+* http://localhost:3000/
+* http://localhost:3000/name/:name -> http://localhost:3000/name/mechthild
--- docker/index.ts
+++ docker/index.ts
@@ -7,11 +7,21 @@ const port = process.env.PORT;
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hallo Welt");
 });
 
-app.get("/meow", (req, res) => {
-  res.send("Meow");
+app.get("/name/:name", (req, res) => {
+  const { name } = req.params;
+  const dateNow = new Date();
+
+  const nameFormatted = `${name.charAt(0).toUpperCase()}${name.slice(1)}`;
+  const dateFormatted = dateNow.toLocaleDateString("de-DE", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+
+  res.send(`Hallo ${nameFormatted}. Heute ist der ${dateFormatted}.`);
 });
 
 app.listen(port, () => {
